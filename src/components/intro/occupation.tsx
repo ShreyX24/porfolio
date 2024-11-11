ca27e3f15c2cc24ee2205b66a33e48ba5369f2a5
ShreyX <134378619+ShreyX24@users.noreply.github.com>
2024-11-11 20:32:08 +0530

    Update occupation.tsx ~ full stack -> frontend developer

--- src/components/intro/occupation.tsx
+++ src/components/intro/occupation.tsx
@@ -5,7 +5,7 @@ export const Occupation = () => {
   return (
     <Slide animdelay={true}>
       <span className="text-[20px] font-semibold md:font-normal md:text-[30px] poppins">
-        <span>~</span>Fullstack Developer
+        <span>~</span>Frontend Developer
       </span>
     </Slide>
   );
